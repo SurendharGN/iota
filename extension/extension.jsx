45d31a4ba690729c3ca51fc0edee935201dfded1
SurendharGN <work.surendhar@gmail.com>
2024-01-06 00:32:56 +0530

    kicking off the project

---
+++ extension/extension.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Extension = () => {
+  return (
+    <div>extension</div>
+  )
+}
+
+export default Extension
--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,37 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import { Link } from 'react-router-dom';
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+    <div className='root'>
+      <header>
+        <h1>Iota.</h1>
+        <div id='links'>
+          <Link to='/downloadExtension'>Download extension </Link>
+          <span>|</span>
+          <Link> Documentation  </Link>
+          <span>|</span>
+          <Link> Github</Link>
+
+        </div>
+        
+      </header>
+      <main id='desc'>
+        <span id="separator"></span>
+        <p>An application to detect, the use, type, and scale of dark patterns on e-commerce platforms.
+          <span >
+            It includes patterns such as
+        User Interface Deception,Subscription Trickery,
+Misleading Product Information,Forced Account Creation, Limited User Choice, Misleading Product Information, Fake Urgency and Scarcity, Hidden Costs.
+          </span>
+        
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      </main>
+    </div>
   )
 }
 
